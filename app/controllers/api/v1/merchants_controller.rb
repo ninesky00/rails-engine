bedb971831c76cbb10aeeb846b583492dcbc10f5
ninesky00 <jiangzho@gmail.com>
2021-02-13 13:34:45 -0600

    add index, show, create, update, destroy tests and action for merchants

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,27 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: Merchant.all
   end
+
+  def show
+    render json: Merchant.find(params[:id])
+  end
+
+  def create
+    Merchant.create(merchant_params)
+  end
+
+  def update
+    Merchant.update(params[:id], merchant_params)
+  end
+
+  def destroy
+    Merchant.destroy(params[:id])
+  end
+
+  private 
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
+
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,5 +8,71 @@ describe "Merchants API" do
 
     expect(response).to be_successful
     merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
+  end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+  
+    get "/api/v1/merchants/#{id}"
+  
+    merchant = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(response).to be_successful
+  
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+  
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
+
+  it "can create a new merchant" do
+    merchant_params = ({
+                    name: 'Express',
+                  })
+    headers = {"CONTENT_TYPE" => "application/json"}
+  
+    post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
+    created_merchant = Merchant.last
+  
+    expect(response).to be_successful
+    expect(created_merchant.name).to eq(merchant_params[:name])
+  end
+
+  it "can update an existing book" do
+    id = create(:merchant).id
+    previous_name = Merchant.last.name
+    merchant_params = { name: "Amani" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+  
+    # We include this header to make sure that these params are passed as JSON rather than as plain text
+    patch "/api/v1/merchants/#{id}", headers: headers, params: JSON.generate({merchant: merchant_params})
+    merchant = Merchant.find_by(id: id)
+  
+    expect(response).to be_successful
+    expect(merchant.name).to_not eq(previous_name)
+    expect(merchant.name).to eq("Amani")
+  end
+
+  it "can destroy an book" do
+    merchant = create(:merchant)
+  
+    expect(Merchant.count).to eq(1)
+  
+    delete "/api/v1/merchants/#{merchant.id}"
+  
+    expect(response).to be_successful
+    expect(Merchant.count).to eq(0)
+    expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
